b9185d78cc14a569320f3cfd8e6d9ad7b600530a
ted <zhongguojym@gmail.com>
2017-12-05 20:47:20 +0800

    add: comments sort

--- src/components/CommentList/index.js
+++ src/components/CommentList/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import propTypes from 'prop-types';
+import { Radio } from 'antd';
+import sortBy from 'sort-by';
 
 import CommentItem from '../common/CommentItem';
 
@@ -20,12 +22,39 @@ class CommentList extends React.Component {
     comments: propTypes.array.isRequired,
   };
 
+  state = {
+    sort: 'latest',
+  };
+
+  handleSortChange = (e) => {
+    this.setState({ sort: e.target.value });
+  };
+
   render() {
+    const { sort } = this.state;
     const { comments } = this.props;
+
+    if (sort === 'latest') {
+      comments.sort(sortBy('-timestamp', '-voteScore'));
+    } else if (sort === 'votes') {
+      comments.sort(sortBy('-voteScore', '-timestamp'));
+    } else if (sort === 'oldest') {
+      comments.sort(sortBy('timestamp', '-voteScore'));
+    }
     return (
-      <ol className="comment-list">
-        {comments.map(comment => <CommentItem key={comment.id} comment={comment} />)}
-      </ol>
+      <div>
+        <div className="sort">
+          <span>Sort by: </span>
+          <Radio.Group value={sort} onChange={this.handleSortChange}>
+            <Radio.Button value="latest">Latest</Radio.Button>
+            <Radio.Button value="votes">Votes</Radio.Button>
+            <Radio.Button value="oldest">Oldest</Radio.Button>
+          </Radio.Group>
+        </div>
+        <ol className="comment-list">
+          {comments.map(comment => <CommentItem key={comment.id} comment={comment} />)}
+        </ol>
+      </div>
     );
   }
 }
